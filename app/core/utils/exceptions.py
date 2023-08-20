6c9ba0e517bf3af4b12c0461f6143c0a2fdc45c5
Natacha Bertin <32799433+natachabertin@users.noreply.github.com>
2023-08-20 17:49:45 -0300

    msg added to exception to diff err cases

--- app/core/utils/exceptions.py
+++ app/core/utils/exceptions.py
@@ -1,3 +1,5 @@
 class UnsolvableException(Exception):
     """Triggered when can't be solved."""
-    pass
+    def __init__(self, message):
+        self.message = message
+        super(UnsolvableException, self).__init__(message)
