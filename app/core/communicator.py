94f2286c8bb70617beee7d9cda79a0c06b17ccbf
Natacha Bertin <32799433+natachabertin@users.noreply.github.com>
2023-08-26 17:20:26 -0300

    Comm uncoupling refactor

--- app/core/communicator.py
+++ app/core/communicator.py
@@ -3,16 +3,17 @@ from core.checker import Checker
 
 class Communicator:
     """Interacts between core and with API/CLI
-    Receives params as integers.
+    Receives params as integers; and the class Checker,
+    which is the enter point to the orchestration logic.
     Returns results object.
     """
 
-    def __init__(self, jar_x, jar_y, goal):
-        self.result = Checker(jar_x, jar_y, goal).report()
+    def __init__(self, jar_x, jar_y, goal, checker):
+        self.result = checker(jar_x, jar_y, goal).report()
 
     def report(self):
         return self.result
 
 
 if __name__ == "__main__":
-    print(Communicator(5, 4, 2).report())
+    print(Communicator(5, 4, 2, Checker).report())
