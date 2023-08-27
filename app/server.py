8aa3f8d70d8d7251fb774e777a569ffa8ef0ae1b
Natacha Bertin <32799433+natachabertin@users.noreply.github.com>
2023-08-27 18:36:10 -0300

    server: expose api for GET params as query and returning

--- app/server.py
+++ app/server.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 
+from core.board import Juggler
+from core.checker import Checker
+
 app = FastAPI()
 
 
 @app.get("/")
-def hello_world():
-    return {"message": "OK"}
+def index():
+    return {"Water Jug Riddle": "Ready"}
+
+
+@app.get("/riddle/")
+async def read_item(x: int, y: int, z: int):
+    return Checker(x, y, z, Juggler).report()
