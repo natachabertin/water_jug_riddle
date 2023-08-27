62fd881da1c33574614758da79d671be17a431f7
Natacha Bertin <32799433+natachabertin@users.noreply.github.com>
2023-08-26 22:04:01 -0300

    linter and clean unused code

--- app/core/board.py
+++ app/core/board.py
@@ -1,6 +1,4 @@
 from collections import deque
-from math import gcd
-from operator import itemgetter
 
 from core.utils.enums import Status
 from core.utils.exceptions import UnsolvableException
@@ -18,7 +16,7 @@ class Jug:
         return f"{self.name} ({self.capacity} gal):{self.content} "
 
     def __repr__(self):
-        return f"<Jug {self.status.name} // {self.content} of {self.capacity} ({self.space} free)>"
+        return f"<Jug {self.status.name} // {self.content} of {self.capacity}>"
 
     def _update_content(self, amount):
         """Updates content and space consistently.
@@ -52,181 +50,14 @@ class Jug:
 
 class Juggler:
     def __init__(self, jar_x, jar_y, goal):
-        # TODO: move this out of juggler (don't call if there is a result or a no result)
-
-        try:
-            solution = self._was_solved(jar_x, jar_y, goal)
-            if solution:
-                self.ok_solution_response(solution)
-                # TODO: yeah, this must be refact
-            self._is_solvable(jar_x, jar_y, goal)
-        except UnsolvableException as e:
-            raise
-        else:
-            self.jar_x = Jug(jar_x, "Jar-X")
-            self.jar_y = Jug(jar_y, "Jar-Y")
-            self.goal = goal
+        self.jar_x = Jug(jar_x, "Jar-X")
+        self.jar_y = Jug(jar_y, "Jar-Y")
+        self.goal = goal
 
     def __repr__(self):
         return f"<Juggler [{self.jar_x}, {self.jar_y}] - {self.goal}>"
 
-    def no_solution_response(self, reason):
-        # TODO: move to some communicator class
-        return f"No Solution: {reason}"
-
-    def ok_solution_response(self, solution):
-        # TODO: move to some communicator class
-        return solution
-
-    def fill(self, jar):
-        """Fills jar"""
-        jar.fill()
-
-    def empty(self, jar):
-        """Empties jar"""
-        jar.empty()
-
-    def transfer(self, origin, destination):
-        """Checks remaining space
-        and transfers from origin to destination as much as possible"""
-        transferred_water = min([destination.space, origin.content])
-
-        origin._update_content(-transferred_water)
-        destination._update_content(transferred_water)
-
-    def _reset_jars(self):
-        """Empties both jars"""
-        self.jar_x.empty()
-        self.jar_y.empty()
-
-    def _solver_xy(self):
-        # TODO: move to solver (divide the movements from the proper algorithm)
-        """Always move from x to y"""
-        step = 0
-        process = list()
-
-        while self.goal not in (self.jar_x.content, self.jar_y.content):
-            if self.jar_x.content == 0:
-                self.fill(self.jar_x)
-                step += 1
-                process.append(
-                    f"Step {step} : {self.jar_x.content} - {self.jar_y.content}"
-                )
-                continue
-
-            if self.jar_y.content == self.jar_y.capacity:
-                self.empty(self.jar_y)
-                step += 1
-                process.append(
-                    f"Step {step} : {self.jar_x.content} - {self.jar_y.content}"
-                )
-                continue
-
-            self.transfer(self.jar_x, self.jar_y)
-            step += 1
-            process.append(f"Step {step} : {self.jar_x.content} - {self.jar_y.content}")
-        return dict(steps=step, process=process)
-
-    def _solver_yx(self):
-        # TODO: move to solver (divide the movements from the proper algorithm)
-        """Always move from y to x"""
-        step = 0
-        process = list()
-
-        while self.goal not in (self.jar_x.content, self.jar_y.content):
-            if self.jar_y.content == 0:
-                self.fill(self.jar_y)
-                step += 1
-                process.append(
-                    f"Step {step} : {self.jar_x.content} - {self.jar_y.content}"
-                )
-                continue
-
-            if self.jar_x.content == self.jar_x.capacity:
-                self.empty(self.jar_x)
-                step += 1
-                process.append(
-                    f"Step {step} : {self.jar_x.content} - {self.jar_y.content}"
-                )
-                continue
-
-            self.transfer(self.jar_y, self.jar_x)
-            step += 1
-            process.append(f"Step {step} : {self.jar_x.content} - {self.jar_y.content}")
-        return dict(steps=step, process=process)
-
-    def solve_brute_force(self):
-        x_to_y = self._solver_xy()
-        self._reset_jars()  # a proper refactor should be create the jar here each time instead.
-        y_to_x = self._solver_yx()
-        solution = min([x_to_y, y_to_x], key=itemgetter("steps"))
-        return solution
-
-    def _is_solvable(self, jar_x, jar_y, goal):
-        if goal == 0 or (jar_y == 0 and jar_x == 0):
-            raise UnsolvableException("Goal is zero or both jars are.")
-        if self._is_not_goal_div_by_jars_gcd(jar_x, jar_y, goal):
-            raise UnsolvableException("Goal is not divisible by the GCD of both jars.")
-        if self._is_goal_sum_of_both_jars(jar_x, jar_y, goal):
-            raise UnsolvableException(
-                "Goal is the sum of both jars, only can be measured separately"
-            )
-        if self._is_goal_gt_bigger_jar(jar_x, jar_y, goal):
-            raise UnsolvableException(
-                f"Goal is bigger than the bigger jar. No space to hold {goal} gallons."
-            )
-
-    def _is_not_goal_div_by_jars_gcd(self, jar_x, jar_y, goal):
-        """Only is solvable if goal can be divided by the GCD of both jars capacity."""
-        return goal % gcd(jar_x, jar_y) != 0
-
-    def _is_goal_sum_of_both_jars(self, jar_x, jar_y, goal):
-        return jar_x + jar_y == goal
-
-    def _is_goal_gt_bigger_jar(self, jar_x, jar_y, goal):
-        return goal > max(jar_x, jar_y)
-
-    def _was_solved(self, jar_x, jar_y, goal):
-        """NOT IMPLEMENTED
-        If there is time, implement a cache where it saves the results.
-        so you query the DB with the 3 values and if it was solved before,
-        retrieve process from cache instead of calculating again.
-        If can't be resolved, process is null.
-        """
-        solved_before = None
-        """select process
-         from historic
-         where goal = ?goal
-         and jars = ?(jar_x, jar_y)
-         or jars = ?(jar_y, jar_x)
-         """
-        if solved_before and not solved_before["process"]:
-            raise UnsolvableException()
-        return solved_before
-
-    def solve_queue(self):
-        statuses_to_check = deque()
-        checked_statuses = list((self._current_status(),))
-
-        statuses_to_check.extend(
-            self._get_next_statuses(self._current_status(), checked_statuses)
-        )
-
-        while statuses_to_check:
-            checking_status = statuses_to_check.popleft()
-            checked_statuses.append(checking_status)
-            if self.goal_achieved(checking_status):
-                print(f"solution in {len(checked_statuses)}")
-                print(checked_statuses)
-                # self.ok_solution_response(f"solution in {len(checked_statuses)}")
-                break
-            else:
-                statuses_to_check.extend(
-                    self._get_next_statuses(checking_status, checked_statuses)
-                )
-
     def solve(self):
-        # TODO: try orderdict instead of deque + graph
         statuses_to_check = deque()
         current_status = self._current_status()
         checked_statuses = list((current_status,))
@@ -247,7 +78,6 @@ class Juggler:
             if self.goal_achieved(checking_status):
                 path = self.retrieve_path(graph, checking_status, checked_statuses)
                 return len(path), path
-                break
             else:
                 next_statuses = self._get_next_statuses(
                     checking_status, checked_statuses
@@ -255,7 +85,8 @@ class Juggler:
                 graph.update({checking_status: next_statuses})
                 statuses_to_check.extend(next_statuses)
 
-    def retrieve_path(self, graph, checking_status, checked_statuses):
+    @staticmethod
+    def retrieve_path(graph, checking_status, checked_statuses):
         path = list()
 
         for node in reversed(checked_statuses):
